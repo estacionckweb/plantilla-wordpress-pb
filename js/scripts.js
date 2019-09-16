2ba6af3c33a942e2cc4f771ef846dd477220636f
Nestor Pena <nestor2509@gmail.com>
2019-09-16 11:04:05 -0500

    avances pagina plataforma

--- js/scripts.js
+++ js/scripts.js
@@ -257,6 +257,7 @@
 
         $.getJSON(url, (data) => {
           console.log(data);
+          $('.archive .inside').empty();
           for (var i = 0; i < data.length; i++) {
             if (data[i].acf.galeria) {
               var $div = darBloqueItemObra(data[i].link, data[i].acf.galeria[0].sizes['large'], data[i].title.rendered, data[i].tipos_name, data[i].tipos_color);
