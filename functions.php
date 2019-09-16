4ab15ca92e409ade419fb73c0a4fc4ea682aa0d6
Néstor Andres <nestor2509@gmail.com>
2019-09-16 07:20:20 -0500

    avences pre entrega

--- functions.php
+++ functions.php
@@ -592,4 +592,71 @@ function wpse_287931_get_tipos_names( $object, $field_name, $request ) {
     return $formatted_categories;
 }
 
+if( function_exists('acf_add_local_field_group') ):
+
+    acf_add_local_field_group(array(
+        'key' => 'group_5d7f7c66d21cc',
+        'title' => 'Opciones logo',
+        'fields' => array(
+            array(
+                'key' => 'field_5d7f7c7460132',
+                'label' => 'Imagen',
+                'name' => 'imagen',
+                'type' => 'image',
+                'instructions' => '',
+                'required' => 0,
+                'conditional_logic' => 0,
+                'wrapper' => array(
+                    'width' => '',
+                    'class' => '',
+                    'id' => '',
+                ),
+                'return_format' => 'array',
+                'preview_size' => 'medium',
+                'library' => 'all',
+                'min_width' => '',
+                'min_height' => '',
+                'min_size' => '',
+                'max_width' => '',
+                'max_height' => '',
+                'max_size' => '',
+                'mime_types' => '',
+            ),
+            array(
+                'key' => 'field_5d7f7c7a60133',
+                'label' => 'Color',
+                'name' => 'color',
+                'type' => 'color_picker',
+                'instructions' => '',
+                'required' => 0,
+                'conditional_logic' => 0,
+                'wrapper' => array(
+                    'width' => '',
+                    'class' => '',
+                    'id' => '',
+                ),
+                'default_value' => '',
+            ),
+        ),
+        'location' => array(
+            array(
+                array(
+                    'param' => 'page',
+                    'operator' => '==',
+                    'value' => '7',
+                ),
+            ),
+        ),
+        'menu_order' => 0,
+        'position' => 'normal',
+        'style' => 'default',
+        'label_placement' => 'top',
+        'instruction_placement' => 'label',
+        'hide_on_screen' => '',
+        'active' => true,
+        'description' => '',
+    ));
+    
+    endif;
+
 ?>
